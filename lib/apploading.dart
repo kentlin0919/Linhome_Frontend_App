13eafea6fdd2237dcf9d296388cd464b0e65d9cb
kentlin0919 <kent900919@gmail.com>
2024-02-01 17:58:04 +0800

    feat: 架構簡易的Login頁面

---
+++ lib/apploading.dart
@@ -0,0 +1,15 @@
+import 'package:linhome_frontend_app/import.dart';
+
+class Apploading extends StatefulWidget {
+  const Apploading({super.key});
+
+  @override
+  State<Apploading> createState() => _ApploadingState();
+}
+
+class _ApploadingState extends State<Apploading> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

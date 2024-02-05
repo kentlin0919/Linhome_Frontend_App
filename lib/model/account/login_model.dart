2615f4fc664a1d6e3ade00b42a7c58c2c592b7e8
kentlin0919 <kent900919@gmail.com>
2024-02-05 17:18:31 +0800

    feat: API

--- lib/model/account/login_model.dart
+++ lib/model/account/login_model.dart
@@ -1,7 +1,14 @@
 import 'package:linhome_frontend_app/import.dart';
 
 class LoginModel {
-  Login(String username,String password){
-    return api().post();
+  Login(String username, String password) {
+    var Body;
+    var url;
+    var jwt;
+    return api().postapi(
+      Body,
+      url,
+      jwt,
+    );
   }
 }

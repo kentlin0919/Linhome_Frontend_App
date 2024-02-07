37b867a30dc6b4312c14bf9c1707f87b83c5084e
kentlin0919 <kent900919@gmail.com>
2024-02-07 15:24:14 +0800

    重購: api 相關的Func

--- lib/model/api.dart
+++ lib/model/api.dart
@@ -19,16 +19,96 @@ class api {
       body: jsonEncode(body),
     );
 
-    debugPrint(apiJwtHeader + jwt.toString());
     DateTime endTime = DateTime.now();
     Duration durationInMilliseconds = endTime.difference(startTime);
     debugPrint('${(durationInMilliseconds.inMilliseconds) / 60}秒');
     return response;
   }
 
-  void getapi() {}
+  Future<Response> getapi(
+    dynamic body,
+    String url,
+    String jwt,
+  ) async {
+    DateTime startTime = DateTime.now();
 
-  void deletapi() {}
+    Response response = await get(
+      Uri.parse(apiUrl + url),
+      headers: {
+        "Authorization": 'Bearer ' + apiJwtHeader + jwt.toString(),
+        "Content-Type": "application/json",
+      },
+    );
 
-  void putapi() {}
+    DateTime endTime = DateTime.now();
+    Duration durationInMilliseconds = endTime.difference(startTime);
+    debugPrint('${(durationInMilliseconds.inMilliseconds) / 60}秒');
+    return response;
+  }
+
+  Future<Response> putapi(
+    dynamic body,
+    String url,
+    String jwt,
+  ) async {
+    DateTime startTime = DateTime.now();
+
+    Response response = await put(
+      Uri.parse(apiUrl + url),
+      headers: {
+        "Authorization": 'Bearer ' + apiJwtHeader + jwt.toString(),
+        "Content-Type": "application/json",
+      },
+      body: jsonEncode(body),
+    );
+
+    DateTime endTime = DateTime.now();
+    Duration durationInMilliseconds = endTime.difference(startTime);
+    debugPrint('${(durationInMilliseconds.inMilliseconds) / 60}秒');
+    return response;
+  }
+
+  Future<Response> deletapi(
+    dynamic body,
+    String url,
+    String jwt,
+  ) async {
+    DateTime startTime = DateTime.now();
+
+    Response response = await delete(
+      Uri.parse(apiUrl + url),
+      headers: {
+        "Authorization": 'Bearer ' + apiJwtHeader + jwt.toString(),
+        "Content-Type": "application/json",
+      },
+      body: jsonEncode(body),
+    );
+
+    DateTime endTime = DateTime.now();
+    Duration durationInMilliseconds = endTime.difference(startTime);
+    debugPrint('${(durationInMilliseconds.inMilliseconds) / 60}秒');
+    return response;
+  }
+
+  Future<Response> patchapi(
+    dynamic body,
+    String url,
+    String jwt,
+  ) async {
+    DateTime startTime = DateTime.now();
+
+    Response response = await patch(
+      Uri.parse(apiUrl + url),
+      headers: {
+        "Authorization": 'Bearer ' + apiJwtHeader + jwt.toString(),
+        "Content-Type": "application/json",
+      },
+      body: jsonEncode(body),
+    );
+
+    DateTime endTime = DateTime.now();
+    Duration durationInMilliseconds = endTime.difference(startTime);
+    debugPrint('${(durationInMilliseconds.inMilliseconds) / 60}秒');
+    return response;
+  }
 }
